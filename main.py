00b2213dcd55ba336110b2324d251197ed525000
Bedwyrd <Bedwyrd@users.noreply.github.com>
2021-12-28 04:55:33 +0000

    First version/update

--- main.py
+++ main.py
@@ -5,10 +5,22 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import json
+
+length_of_one_square=3
+total_length=3
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def saveListToFile(listname, pathtosave):
+    with open(pathtosave, 'a') as f:
+        f.write(json.dumps(listname) + "\n")
+
+    # Now read the file back into a Python list object
+    #with open('test.txt', 'r') as f:
+    #    a = json.loads(f.read())
 
 def for_recursive(number_of_loops, range_list, execute_function, current_index=0, iter_list = []):
     #print('number_of_loops = ', number_of_loops, 'range_list = ', range_list)
@@ -23,15 +35,6 @@ def for_recursive(number_of_loops, range_list, execute_function, current_index=0
             for_recursive(number_of_loops, iter_list=iter_list, range_list=range_list, current_index=current_index + 1,
                           execute_function=execute_function)
 
-def saveListToFile(listname, pathtosave):
-    file1 = open(pathtosave,"w")
-    for i in listname:
-        file1.writelines("{}\n".format(i))
-    file1.close()
-
-# to save:
-#saveListToFile(list, path)
-
 def do_whatever(index_list):
     #print('index_list[:] = ', index_list[:])
     index_list_after_2 = index_list[2:]
@@ -40,7 +43,9 @@ def do_whatever(index_list):
 
     if sum(index_list[2:]) == index_list[0] and index_list[1] == len(index_list[2:]) and res:
         #print ('res = ', res)
-        #saveListToFile(list, path)
+        temp_list = [length_of_one_square]
+        index_list = temp_list + index_list
+        saveListToFile(index_list, 'sum_no_of_squares_list')
         return print(index_list)
 
 def create_list(length_of_one_square, total_length):
@@ -146,7 +151,7 @@ if __name__ == '__main__':
     #possib(total, no_of_squares, l_combin=None)
     #possib(13, 3, l_combin=None)
     #print_hi('possib(total, no_of_squares, l_combin=None) =', possib(1, 1, l_combin=None))
-    create_list(length_of_one_square=3, total_length=3)
+    create_list(length_of_one_square, total_length)
 #print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
