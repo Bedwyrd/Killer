2961789a8b02548fa4bbfe91a1971bd5b5903a6a
Bedwyrd <Bedwyrd@users.noreply.github.com>
2021-12-29 04:07:21 +0000

    First version/update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 
-#The Github stuff was done as per https://medium.com/analytics-vidhya/how-to-share-the-pycharm-project-on-github-de837063c12
+# The Github stuff was done as per
+# https://medium.com/analytics-vidhya/how-to-share-the-pycharm-project-on-github-de837063c12
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -16,23 +17,27 @@ total_length = int(value2)
 choice = input("Enter 1 for create_list.\nEnter 2 for specifying Killer game values.:\n")
 choice = int(choice)
 
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 def saveListToFile(listname, pathtosave):
     with open(pathtosave, 'a') as f:
         f.write(json.dumps(listname) + "\n")
 
-def openFileToList(listname, pathtosave):
+
+def openFileToList(pathtosave):
     # Now read the file back into a Python list object
     with open(pathtosave, 'r') as f:
         listname = [json.loads(line) for line in f]
-    #print('listname = ', listname)
+    # print('listname = ', listname)
     return listname
 
-def for_recursive(number_of_loops, range_list, execute_function, current_index=0, iter_list = []):
-    #print('number_of_loops = ', number_of_loops, 'range_list = ', range_list)
+
+def for_recursive(number_of_loops, range_list, execute_function, current_index=0, iter_list=[]):
+    # print('number_of_loops = ', number_of_loops, 'range_list = ', range_list)
     if iter_list == []:
         iter_list = [0] * number_of_loops
 
@@ -44,127 +49,151 @@ def for_recursive(number_of_loops, range_list, execute_function, current_index=0
             for_recursive(number_of_loops, iter_list=iter_list, range_list=range_list, current_index=current_index + 1,
                           execute_function=execute_function)
 
+
 def do_whatever(index_list):
-    #print('index_list[:] = ', index_list[:])
+    # print('index_list[:] = ', index_list[:])
     index_list_after_2 = index_list[2:]
-    #res returns true if the elements of the list are in a strictly increasing order
+    # res returns true if the elements of the list are in a strictly increasing order
     res = all(i < j for i, j in zip(index_list_after_2, index_list_after_2[1:]))
 
     if sum(index_list[2:]) == index_list[0] and index_list[1] == len(index_list[2:]) and res:
-        #print ('res = ', res)
+        # print ('res = ', res)
         temp_list = [length_of_one_square]
         index_list = temp_list + index_list
-        #saveListToFile(index_list, 'sum_no_of_squares_list')
+        # Uncomment following line to append into file sum_no_of_squares_list
+        # saveListToFile(index_list, 'sum_no_of_squares_list')
         return print(index_list)
 
-def create_list(length_of_one_square, total_length):
-    last_number = length_of_one_square**2
+
+def create_list(length_of_one_square):
+    last_number = length_of_one_square ** 2
     total_in_one_square = 0
-    for x in range(1, length_of_one_square**2+1):
+    for x in range(1, last_number + 1):
         print('x = ', x)
         total_in_one_square = total_in_one_square + x
         print('total_in_one_square = ', total_in_one_square)
 
     # total_in_shape, no_of_squares
-    number_of_loops_lv=3
-    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv-1)), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                              range(number_of_loops_lv-2, number_of_loops_lv-1),
-                              range(1, length_of_one_square**2+1)],
+    number_of_loops_lv = 3
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number + 1)],
                   execute_function=do_whatever, number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 4
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function=do_whatever, number_of_loops=number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 5
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 6
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-2),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 2),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 7
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-3),
-                                range(1, length_of_one_square**2-2),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 3),
+                              range(1, last_number - 2),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 8
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-4),
-                                range(1, length_of_one_square**2-3),
-                                range(1, length_of_one_square**2-2),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 4),
+                              range(1, last_number - 3),
+                              range(1, last_number - 2),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 9
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-5),
-                                range(1, length_of_one_square**2-4),
-                                range(1, length_of_one_square**2-3),
-                                range(1, length_of_one_square**2-2),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 5),
+                              range(1, last_number - 4),
+                              range(1, last_number - 3),
+                              range(1, last_number - 2),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 10
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-6),
-                                range(1, length_of_one_square**2-5),
-                                range(1, length_of_one_square**2-4),
-                                range(1, length_of_one_square**2-3),
-                                range(1, length_of_one_square**2-2),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 6),
+                              range(1, last_number - 5),
+                              range(1, last_number - 4),
+                              range(1, last_number - 3),
+                              range(1, last_number - 2),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
     number_of_loops_lv = 11
 
-    for_recursive(range_list=[  range(sum(range(1, number_of_loops_lv-1 )), sum(range(length_of_one_square**2+3-number_of_loops_lv, length_of_one_square**2+1 )) + 1),
-                                range(number_of_loops_lv-2, number_of_loops_lv-1),
-                                range(1, length_of_one_square**2-7),
-                                range(1, length_of_one_square**2-6),
-                                range(1, length_of_one_square**2-5),
-                                range(1, length_of_one_square**2-4),
-                                range(1, length_of_one_square**2-3),
-                                range(1, length_of_one_square**2-2),
-                                range(1, length_of_one_square**2-1),
-                                range(1, length_of_one_square**2),
-                                range(1, length_of_one_square**2+1)], execute_function = do_whatever, number_of_loops = number_of_loops_lv)
+    for_recursive(range_list=[range(sum(range(1, number_of_loops_lv - 1)),
+                                    sum(range(last_number + 3 - number_of_loops_lv, last_number + 1)) + 1),
+                              range(number_of_loops_lv - 2, number_of_loops_lv - 1),
+                              range(1, last_number - 7),
+                              range(1, last_number - 6),
+                              range(1, last_number - 5),
+                              range(1, last_number - 4),
+                              range(1, last_number - 3),
+                              range(1, last_number - 2),
+                              range(1, last_number - 1),
+                              range(1, last_number),
+                              range(1, last_number + 1)], execute_function=do_whatever,
+                  number_of_loops=number_of_loops_lv)
+
 
 def pop_game(length_of_one_square, total_length):
     # saveListToFile(index_list, 'sum_no_of_squares_list')
-    index_list_f = []
-    index_list_f = openFileToList(index_list_f, 'sum_no_of_squares_list')
+    index_list_f = openFileToList('sum_no_of_squares_list')
     print('index_list_f = ', index_list_f)
+    l_f_length_of_one_square = [x for x in index_list_f if x[0] == length_of_one_square]
+    print('l_f_length_of_one_square = ', l_f_length_of_one_square)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     if choice == 1:
-        print(f'You entered length_of_one_square = {length_of_one_square} and total_length = {total_length} for create_list')
-        create_list(length_of_one_square, total_length)
+        print(
+            f'You entered length_of_one_square = {length_of_one_square} and total_length = {total_length} for create_list')
+        create_list(length_of_one_square)
     elif choice == 2:
-        print(f'You entered length_of_one_square = {length_of_one_square} and total_length = {total_length} for Killer values')
+        print(
+            f'You entered length_of_one_square = {length_of_one_square} and total_length = {total_length} for Killer values')
         pop_game(length_of_one_square, total_length)
     else:
         print("Wrong Choice, terminating the program.")
 
-#print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+# print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
